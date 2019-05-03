f63decae665db2b682bc19e6b41b6366e66c7715
Chatchawan Kotarasu <IamMitsuo@users.noreply.github.com>
2019-05-04 00:39:47 +0700

    Update qrcode_generator.py

--- qrcode_generator.py
+++ qrcode_generator.py
@@ -13,4 +13,9 @@ def generate_qr():
     info['uuid'] = "94f39d29-7d6d-437d-973b-fba39e49d4ee"
     info_str = json.dumps(info, indent=4)
     url = pyqrcode.create(info_str)
-    url.png('')
+    url.png('masai-qr-code.png', scale=8)
+    print("Printing QR Code")
+    print(url.terminal())
+
+if __name__ == "__main__":
+    generate_qr()
