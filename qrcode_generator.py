3e6664aa734ca8b05ce6488cf061f27acc5b4c88
Chatchawan Kotarasu <IamMitsuo@users.noreply.github.com>
2019-05-04 01:00:35 +0700

    Update qrcode_generator.py

--- qrcode_generator.py
+++ qrcode_generator.py
@@ -9,7 +9,9 @@ import bluetooth
 def generate_qr():
     info = {}
     info['name'] = socket.gethostname()
-    info['address'] = bluetooth.read_local_bdaddr()
+    address = bluetooth.read_local_bdaddr()
+    if type(address) is list:
+        info['address'] = address.pop()
     info['uuid'] = "94f39d29-7d6d-437d-973b-fba39e49d4ee"
     info_str = json.dumps(info, indent=4)
     url = pyqrcode.create(info_str)
